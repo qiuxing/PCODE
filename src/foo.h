576e6b814323e79e9d1021878357e4db3843ef80
Xing Qiu <xing.qiu@gmail.com>
2012-10-18 21:29:50 -0400

    deleted some files in src/.

--- src/foo.h
+++
@@ -1,7 +0,0 @@
-/* A header file */
-
-#include <float.h>
-
-/* void read_infile(char *filename,GENE_DATA *pdata); */
-/* float two_sample_tstat(const float *Y, const int* L,const int n, */
-/* 		       const float na,const void *extra);  */
